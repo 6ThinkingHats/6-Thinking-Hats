fbaa3ed6b357237da87fba53063e1937f23843e1
오성준 <osj2387@naver.com>
2022-08-06 14:02:27 +0900

    [DOCS] Delete FE files in be branch

--- 6hatsfe/src/api/http.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-axios.defaults.withCredentials = true;
-export default axios.create({
-  baseURL: "http://i7a709.p.ssafy.io:8081",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
